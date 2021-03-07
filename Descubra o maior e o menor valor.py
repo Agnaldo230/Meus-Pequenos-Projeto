fbe5da62ee2747e20ecd84e234b3937122643476
Agnaldo230 <79926170+Agnaldo230@users.noreply.github.com>
2021-03-07 08:30:10 -0300

    Add files via upload
    Descubra o maior e o menor valor

---
+++ Descubra o maior e o menor valor.py
@@ -0,0 +1,9 @@
+a = int(input('Digite o primeiro valor: '))
+b = int(input('Digite o segundo valor: '))
+c = int(input('Digite o terceiro valor: '))
+d = int(input('Digite o quarto valor: '))
+valor = [a, b, c, d]
+print('O menor valor foi o {}: '.format(min(valor)))
+print('O maior valor foi o {}: '.format(max(valor)))
+
+
